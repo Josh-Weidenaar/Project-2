5f0b71cfda24ef0ce24f34b04a055b0683cf4227
Josh Weidenaar <josh.weidenaar@intersectillinois.org>
2021-06-22 20:03:51 -0500

    adding stock heroku files to repo for group

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/beard_db"
 mongo = PyMongo(app)
 
+@app.route("/")
+def home():
+    return render_template("index.html")
+
 @app.route("/api/data/all")
 def returndata():
     documents = mongo.db.book_gender.find()
