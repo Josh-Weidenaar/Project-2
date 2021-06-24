50c0e54c054bbbf85d3d07407a52452c138126e9
Josh Weidenaar <josh.weidenaar@intersectillinois.org>
2021-06-24 18:03:47 -0500

    button working

---
+++ static/js/tables.js
@@ -0,0 +1,26 @@
+
+// ('your-custom-id').mdbDropSearch();
+const searchInputDropdown = document.getElementById('search-input-dropdown');
+const dropdownOptions = document.querySelectorAll('dropdown-item');
+
+searchInputDropdown.addEventListener('input', () => {
+  const filter = searchInputDropdown.value.toLowerCase();
+  showOptions();
+  const valueExist = !!filter.length;
+
+  if (valueExist) {
+    dropdownOptions.forEach((el) => {
+      const elText = el.textContent.trim().toLowerCase();
+      const isIncluded = elText.includes(filter);
+      if (!isIncluded) {
+        el.style.display = 'none';
+      }
+    });
+  }
+});
+
+const showOptions = () => {
+  dropdownOptions.forEach((el) => {
+    el.style.display = 'flex';
+  })
+}
