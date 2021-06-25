33f15759ee4fcc2f4c87c9ca68e7fea6d3d7a262
Josh Weidenaar <josh.weidenaar@intersectillinois.org>
2021-06-24 20:59:01 -0500

    Gender counts uploaded

--- static/js/testapp.js
+++ static/js/testapp.js
@@ -82,7 +82,9 @@ function init() {
         2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,
         2020];
     var femme = []
-        
+    var masc = [] 
+    var nullielist = []
+    var _3rdGenderList = []   
     d3.json("static/data_collection/Beard_db_final.json").then((data)=> {
         console.log("this dat")
         console.log(data)
@@ -90,22 +92,39 @@ function init() {
 
         years.forEach(date => {
             let female = 0;
+            let male = 0;
+            let nullie = 0;
+            let _3rdGender = 0;
             yearArray = data.values.filter(i => i.year == date)
             yearArray.forEach(j => {
                 var gender = j.gender;
                 var year = j.year;
 
                 if (gender === "Female"){
-                    female += 1;}
+                    female += 1;
+                } else if (gender === "Male"){
+                    male += 1; 
+                } else if (gender === null){
+                    nullie += 1;
+                } else {
+                    _3rdGender += 1;
+                }
                 });
+            masc.push(male);
             femme.push(female);
-            console.log(femme);
-            console.log(female);
+            nullielist.push(nullie);
+            _3rdGenderList.push(_3rdGender);
+            // console.log("femme");
+            // console.log(femme);
+            // console.log("masc");
+            // console.log(masc);
+            // console.log("nullie" + (nullielist));
+            // console.log(_3rdGenderList);
             });
-
-
-        });
         
+        
+        });
+
         // get the id data to the dropdwown menu
         data.values.forEach(function(name) {
             dropdown.append("option").text(name['achievement status']).property("value");
